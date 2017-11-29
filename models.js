bd1dffe8aadcb46e34611c3c97cb3705cad355c7
Nicole Gilbert <nicolegilbert16@gmail.com>
2017-11-29 13:45:23 -0800

    added third argument to models

--- models.js
+++ models.js
@@ -27,6 +27,6 @@ blogPostSchema.methods.apiRepr = function() {
   };
 };
 
-const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+const BlogPost = mongoose.model('BlogPost', blogPostSchema, 'blog-posts');
 
 module.exports = {BlogPost};
