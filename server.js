e722b5a0cb36fabc256bdfb08cc455c1f491dda6
Nicole Gilbert <nicolegilbert16@gmail.com>
2017-11-30 14:41:45 -0800

    added auth middleware

--- server.js
+++ server.js
@@ -128,33 +128,36 @@ app.get('/posts/:id', (req, res) => {
     });
 });
 
-app.post('/posts', (req, res) => {
-  const requiredFields = ['title', 'content', 'author'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
+app.post('/posts', 
+  //passport.authenticate middleware to protect endpoint
+  localAuth,
+  (req, res) => {
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i=0; i<requiredFields.length; i++) {
+      const field = requiredFields[i];
+      if (!(field in req.body)) {
+        const message = `Missing \`${field}\` in request body`;
+        console.error(message);
+        return res.status(400).send(message);
+      }
     }
-  }
 
-  BlogPost
-    .create({
-      title: req.body.title,
-      content: req.body.content,
-      author: req.body.author
-    })
-    .then(blogPost => res.status(201).json(blogPost.apiRepr()))
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({error: 'Something went wrong'});
-    });
+    BlogPost
+      .create({
+        title: req.body.title,
+        content: req.body.content,
+        author: req.body.author
+      })
+      .then(blogPost => res.status(201).json(blogPost.apiRepr()))
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({error: 'Something went wrong'});
+      });
 
-});
+  });
 
 
-app.delete('/posts/:id', (req, res) => {
+app.delete('/posts/:id', localAuth, (req, res) => {
   BlogPost
     .findByIdAndRemove(req.params.id)
     .then(() => {
@@ -167,7 +170,7 @@ app.delete('/posts/:id', (req, res) => {
 });
 
 
-app.put('/posts/:id', (req, res) => {
+app.put('/posts/:id', localAuth, (req, res) => {
   if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
     res.status(400).json({
       error: 'Request path id and request body id values must match'
@@ -189,7 +192,7 @@ app.put('/posts/:id', (req, res) => {
 });
 
 
-app.delete('/:id', (req, res) => {
+app.delete('/:id', localAuth, (req, res) => {
   BlogPost
     .findByIdAndRemove(req.params.id)
     .then(() => {
